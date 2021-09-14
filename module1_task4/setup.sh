4af5b92ed38f200bf678de2b3512611913a06462
Diego <diego.barto14@gmail.com>
2021-09-14 15:07:46 -0300

    fix setup.sh

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y wget make
 wget https://github.com/gohugoio/hugo/releases/download/v0.88.1/hugo_0.88.1_Linux-64bit.deb
 dpkg -i hugo_0.88.1_Linux-64bit.deb
 rm hugo_0.88.1_Linux-64bit.deb
