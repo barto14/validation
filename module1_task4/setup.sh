65cd5afedcc6016de6eae236e72b6c995b0c430d
Diego <diego.barto14@gmail.com>
2021-09-14 15:05:14 -0300

    se agrega module1_task4

---
+++ module1_task4/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update && apt-get install -y hugo make
+wget https://github.com/gohugoio/hugo/releases/download/v0.88.1/hugo_0.88.1_Linux-64bit.deb
+dpkg -i hugo_0.88.1_Linux-64bit.deb
+rm hugo_0.88.1_Linux-64bit.deb
+make build
